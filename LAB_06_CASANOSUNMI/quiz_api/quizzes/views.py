2983e71855d22fbcac16438019066e7c76986240
Sunmi-CS <sunmicasano@gmail.com>
2025-09-24 16:05:23 -0500

    .

--- LAB_06_CASANOSUNMI/quiz_api/quizzes/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Quiz
-from .serializers import QuizDetailSerializer
- 
-class QuizViewSet(viewsets.ModelViewSet):
-    queryset = Quiz.objects.all()
-    serializer_class = QuizDetailSerializer
-    

ab49f0f834d9e6ffcfb285e7877b2b43668f0e6c
Sunmi-CS <sunmicasano@gmail.com>
2025-10-15 13:45:09 -0500

    commit

--- LAB_09_CASANOSUNMI/cinespoilers/src/App.tsx
+++ LAB_09_CASANOSUNMI/cinespoilers/src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type { Movie } from "./shared/types/movie.types";
+import Footer from "./ui/components/layouts/Footer";
+import Header from "./ui/components/layouts/Header";
+import Hero from "./ui/components/modules/Hero";
+import MovieGrid from "./ui/components/modules/MovieGrid";
+import { getMoviesFromMockData } from "./utils/movie.utils";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const movies: Movie[] = getMoviesFromMockData();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <main className="main">
+        <Hero />
+        <MovieGrid movies={movies} />
+      </main>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ LAB_09_CASANOSUNMI/cinespoilers/src/ui/components/modules/Hero.tsx
@@ -0,0 +1,30 @@
+const Hero = () => {
+  return (
+    <article className="hero">
+      <div className="container d-flex f-direction-column a-items-center g-4">
+        <h1 className="hero__title t-align-center">
+          🎥 Explore spoiler-free cinema with <span className="c-primary">AI-Powered insights ✨</span>
+        </h1>
+        <p className="hero__paragraph t-align-center">
+          👋 Welcome to <span className="f-weight-700">Cine SpoilerS</span> 🍿 where you can see everything about movies without the spoilers 🚫. Discover premieres 🌟, reviews 📝, and interactive AR experiences 🔍
+        </p>
+        <div className="d-flex g-4">
+          <a
+            href="#now-showing"
+            className="button button--primary interactive interactive--xl"
+          >
+            🎬 Now Showing
+          </a>
+          <a
+            href="#coming-soon"
+            className="button button--outline-primary interactive interactive--xl"
+          >
+            🔮 Coming Soon
+          </a>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Hero;
